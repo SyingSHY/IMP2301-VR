324933a44aec69dd8029db8959344d80326fa343
DESKTOP-PH6CO38\stock <tmddnrkfka0@naver.com>
2023-06-03 20:17:49 +0900

    adding stairside wall and adjusting gamamanage script

--- Assets/Scripts/gamamanage.cs
+++ Assets/Scripts/gamamanage.cs
@@ -17,6 +17,8 @@ public class gamamanage : MonoBehaviour
     public GameObject shotgundoor; //access to shotgundoor to destroy
     public GameObject escapedoor; //access to escapedoor to destroy
 
+    private bool isbulb = false; //bool variables for instantiating. script should only instantiate one time at each time.
+
     private int bulbcount = 0; //when player get the bulb and put it. the bulbcount will go up.
     // Start is called before the first frame update
     void Start()
@@ -28,21 +30,29 @@ public class gamamanage : MonoBehaviour
     void Update()
     {
 
-        
-        if(Input.GetKeyDown("t")) //시험용으로 만들어놓은 부분입니다. t를 누르면 전구를 한개 넣는다고 생각해주세요!
+        //나중에 지울부분
+        if (Input.GetKeyDown("t")) //시험용으로 만들어놓은 부분입니다. t를 누르면 전구를 한개 넣는다고 생각해주세요! XR오리진이 없어서 테스트에 필요한 input을 넣은 겁니다! 실제 스크립트에서는 이부분은 지워야합니다.
         {
-           bulbcount++;
-
-            if (bulbcount == 1) onecount();
-            else if(bulbcount == 2) twocount();
-            else if(bulbcount == 3) threecount();
-            else if (bulbcount == 4) fourcount();
-            else if (bulbcount == 5) fivecount();
-            else if (bulbcount == 6) sixcount();
+            socketeventcalling();  // 이함수는 소켓 인터랙션의 이벤트로만 불러오는 함수입니다!
         }
+        // 나중에 지울 부분
+
+
+
+        if (bulbcount == 1 && isbulb) onecount();
+        else if(bulbcount == 2 && isbulb) twocount();
+        else if(bulbcount == 3 && isbulb) threecount();
+        else if (bulbcount == 4 && isbulb) fourcount();
+        else if (bulbcount == 5 && isbulb) fivecount();
+        else if (bulbcount == 6 && isbulb) sixcount();
+        
     }
 
-
+    public void socketeventcalling() //event call by socket interaction. when this method happens. the bulbcount go up.
+    {
+           bulbcount++; //소켓 인터렉션에 이벤트로 전구를 넣으면 이 메소드, socketeventcalling를 불러올 겁니다. 메소드를 불러서 bulbcount가 올라가면 Update()에서 각 상황에 맞는 메소드들을 실행할 겁니다!
+            isbulb= true;
+    }
     private void zerocount() //when the game start. make sound trigger boxes.
     {
         Debug.Log("im 0");
@@ -62,6 +72,7 @@ public class gamamanage : MonoBehaviour
 
         putbulb.Play();
 
+        isbulb= false;
 
     }
 
@@ -75,6 +86,8 @@ public class gamamanage : MonoBehaviour
 
         putbulb.Play();
 
+        isbulb = false;
+
     }
 
     private void threecount()
@@ -85,11 +98,13 @@ public class gamamanage : MonoBehaviour
 
         Instantiate(bulb, new Vector3(-6.5f, 1f, 7.5f), Quaternion.identity); // 4bulb, seond maze
 
-        Destroy(shotgundoor);
+        Destroy(shotgundoor); //door to the shotgun is open. player put the half of the bulb.
 
         ahh.Play();
+        
+        isbulb = false;
 
-       
+
     }
 
     private void fourcount()
@@ -103,7 +118,9 @@ public class gamamanage : MonoBehaviour
 
         putbulb.Play();
 
-        Instantiate(shotgun, new Vector3(0,1f,0), Quaternion.identity);
+        Instantiate(shotgun, new Vector3(0,1f,0), Quaternion.identity); //instantiate shotgun in the middle of the map. because player can only use shotgun once.
+
+        isbulb = false;
 
 
     }
@@ -117,7 +134,9 @@ public class gamamanage : MonoBehaviour
 
         putbulb.Play();
 
-        Instantiate(shotgun, new Vector3(0, 1f, 0), Quaternion.identity);
+        Instantiate(shotgun, new Vector3(0, 1f, 0), Quaternion.identity); //instantiate shotgun in the middle of the map. because player can only use shotgun once.
+
+        isbulb = false;
 
 
 
@@ -126,13 +145,15 @@ public class gamamanage : MonoBehaviour
 
     private void sixcount()
     {
-        Debug.Log("im 6");
+        Debug.Log("im 6"); //put every bulb in the spot.
         
         Instantiate(breeze, new Vector3(-0.5f, 0, 6f), Quaternion.identity);
 
-        Destroy(escapedoor);
+        Destroy(escapedoor); //exit is open
 
         ahh.Play();
+
+        isbulb = false;
 
     }
 }
