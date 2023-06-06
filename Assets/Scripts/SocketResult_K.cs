f8da74dbc26ee0a08bc7c6cdb4bb52fffc113ec3
공준혁 <elbialto31@ajou.ac.kr>
2023-06-07 04:45:34 +0900

    player(2)

--- Assets/Scripts/SocketResult_K.cs
+++ Assets/Scripts/SocketResult_K.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SocketResult_K : MonoBehaviour
 {
+
+
+    public GameObject RedDoor;
+    public GameObject BlueDoor;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class SocketResult_K : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        //if (SocketInteraction.bulbcount >= 3) { }
+        //if (SocketInteraction.bulbcount >= 6) {
+        //    Destroy(socketResult);
+        //}
     }
+
+    private void FixedUpdate()
+    {
+         if (SocketInteraction.bulbcount >= 3) {
+            Destroy(BlueDoor);
+        }
+        if (SocketInteraction.bulbcount >= 6) {
+            Destroy(RedDoor);
+}
+    }
+    
 }
