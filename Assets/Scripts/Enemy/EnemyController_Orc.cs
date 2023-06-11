3f709b9bd9201982e83ee22c2f762d483b518bd7
SyingSHY <posti1x38@gmail.com>
2023-06-12 08:51:47 +0900

    Add enemy sounds and details

--- Assets/Scripts/Enemy/EnemyController_Orc.cs
+++ Assets/Scripts/Enemy/EnemyController_Orc.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.XR.Interaction.Toolkit;
 using static UnityEngine.GraphicsBuffer;
 
 public class EnemyController_Orc : MonoBehaviour
@@ -9,8 +10,12 @@ public class EnemyController_Orc : MonoBehaviour
     // Bulb collection counter to check game progress
     // 게임 진행 상황 확인을 위한 수집된 전구 카운터
     public int levelCounter = 0;
+    public gamamanage gamamanageScript;
 
-    [SerializeField] Transform agentTarget;
+    [SerializeField] private Transform agentTarget;
+    [SerializeField] private GameObject cameraPlayer;
+    [SerializeField] private GameOverFadeOut gameoverFade;
+    [SerializeField] private AudioSource screamSound;
     // GunShooted 테스트용 변수
     [SerializeField] private bool GunshootTest = false;
     [SerializeField] private float startChaseRange = 2.0f;
@@ -18,16 +23,19 @@ public class EnemyController_Orc : MonoBehaviour
 
     private Animator animator;
     private NavMeshAgent navMeshAgent;
+    private AudioSource annoyingSound;
     private int isWalkingHash, isStunnedHash, isLightShootedHash, isGunShootedHash, isGameWinHash, isCatchPlayerHash;
     private float distBtwPlayer;
     private bool isChasing = false;
     private bool isCatchPlayer;
+    private bool isGameOverred = false;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         navMeshAgent = GetComponent<NavMeshAgent>();
+        annoyingSound = GetComponent<AudioSource>();
 
         isWalkingHash = Animator.StringToHash("isWalking");
         isLightShootedHash = Animator.StringToHash("isLightShooted");
@@ -40,6 +48,8 @@ public class EnemyController_Orc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        levelCounter = gamamanageScript.bulbcount;
+
         bool isWalking = animator.GetBool(isWalkingHash);
         bool isLightShooted = animator.GetBool(isLightShootedHash);
         bool isStunned = animator.GetBool(isStunnedHash);
@@ -50,9 +60,20 @@ public class EnemyController_Orc : MonoBehaviour
         // Check whether game has ended by Player's win
         if (levelCounter == 6)
         {
+            animator.SetBool("isGameWin", true);
             isGameWin = true;
             navMeshAgent.speed = 0f;
             return;
+        }
+
+        // Let Player watch enemy biting him/her
+        // Player's display became darker and darker
+        // When it became fully dark, move to GameOver scene
+        if (isGameOverred)
+        {
+            isChasing = false;
+            navMeshAgent.speed = 0f;
+            cameraPlayer.transform.LookAt(transform);
         }
 
         // Enemy running animation & agent speed setting
@@ -71,13 +92,14 @@ public class EnemyController_Orc : MonoBehaviour
             animator.SetBool("isWalking", true);
             navMeshAgent.speed = 1.2f;
         }
+        else if (levelCounter == 6 && isGunShooted == false && isChasing == true)
+        {
+            animator.SetBool("isGameWin", true);
+        }
         else
         {
             navMeshAgent.speed = 0f;
         }
-
-        // Only play animation once for caught by enemy
-        if (isCatchPlayer) isCatchPlayer = !isCatchPlayer;
 
         // Test line for gun shoot
         if (GunshootTest) GunShooted();
@@ -89,13 +111,14 @@ public class EnemyController_Orc : MonoBehaviour
         // Checking whether Player is in the tracking range.
         distBtwPlayer = Vector3.Distance(agentTarget.position, transform.position);
 
-        if (levelCounter < 4)
+        if (levelCounter < 5)
         {
-            if (distBtwPlayer < 2)
+            if (distBtwPlayer < 2 && levelCounter > 1)
             {
+                annoyingSound.Play();
                 isChasing = true;
             }
-            else if (distBtwPlayer > 5)
+            else if (distBtwPlayer > 5 && levelCounter > 1)
             {
                 isChasing = false;
             }
@@ -103,7 +126,9 @@ public class EnemyController_Orc : MonoBehaviour
         else isChasing = true;
 
         // Chasing Player if he/she is in the range
-        if (isChasing) { navMeshAgent.SetDestination(agentTarget.position); }
+        if (isChasing) { 
+            navMeshAgent.SetDestination(agentTarget.position); 
+        }
     }
 
     // Called by SendMessage() : Enemy got shooted by Player
@@ -134,17 +159,27 @@ public class EnemyController_Orc : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && !isGameOverred)
         {
-            isCatchPlayer = true;
+            isCatchPlayer = animator.GetBool(isCatchPlayerHash);
 
-            // Shut down control of Player
-            collision.transform.parent = transform;
-            // 병합 후에 작성 예정
+            cameraPlayer.GetComponent<ActionBasedContinuousMoveProvider>().enabled = false;
+            cameraPlayer.GetComponent<ActionBasedContinuousTurnProvider>().enabled = false;
+
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isCatchPlayer", true);
+            isGameOverred = true;
+            isChasing = false;
 
             // 게임오버 씬으로 이동
             Debug.Log("You've caught by enemy. Game Over.");
-            // 추후 작성 예정
+            gameoverFade.SendMessage("StartFadeOutAnim");
+            screamSound.Play();
+        }
+
+        if (collision.gameObject.tag == "Bullet")
+        {
+            GunShooted();
         }
     }
 }
