56b753f8da6dbbf387e885e0702603c462dd1b70
DESKTOP-PH6CO38\stock <tmddnrkfka0@naver.com>
2023-06-11 22:46:15 +0900

    UIscene XR simulator disable Fixing Lightbulb prefab to able grab fixing lever material adjusting socket interactor script change game title change opening sound effect when player win. Return to main scene fixing Objects general y position etc...

--- Assets/Scripts/HandPresence_K.cs
+++ Assets/Scripts/HandPresence_K.cs
@@ -5,41 +5,35 @@ using UnityEngine.XR;
 
 public class HandPresence_K : MonoBehaviour
 {
-
     private InputDevice targetDevice;
 
- 
-
-    private GameObject spawnedController;
 
 
     [SerializeField]
-    private Animator handAnimator;
-
     private InputDeviceCharacteristics deviceCharacteristics;
 
-   
 
     [SerializeField]
     private GameObject handPrefab;
+
     private GameObject spawnedHand;
+
+    private Animator handAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        InitializedInput();
+        InitializeInput();
+
+
     }
 
-    private void InitializedInput()
+    private void InitializeInput()
     {
-        //list all connected XR devices
         List<InputDevice> devices = new List<InputDevice>();
 
-        InputDeviceCharacteristics devChars = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right;
-        InputDevices.GetDevicesWithCharacteristics(devChars, devices);
 
-        //foreach(InputDevice dev in devices) {
-        //    Debug.Log(dev.name + ", " +dev.characteristics);
-        //}
+        InputDevices.GetDevicesWithCharacteristics(deviceCharacteristics, devices);
 
         if (devices.Count > 0)
         {
@@ -51,6 +45,8 @@ public class HandPresence_K : MonoBehaviour
             spawnedHand = Instantiate(handPrefab, transform);
             handAnimator = spawnedHand.GetComponent<Animator>();
         }
+
+
     }
 
     // Update is called once per frame
@@ -58,58 +54,35 @@ public class HandPresence_K : MonoBehaviour
     {
         if (!targetDevice.isValid)
         {
-            InitializedInput();
+            InitializeInput();
             return;
         }
-        //if (Input.GetKeyDown("space")) {
-        //    InitializedInput();
-        //}
-        // has targetDevice been set yet?
-        if (targetDevice.isValid)
-        {
-            //testInput();
-        }
+
+        UpdateHandAnimation();
+
     }
 
-    private void UpdateAnimator()
+    private void UpdateHandAnimation()
     {
-        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggervalue))
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
         {
-            handAnimator.SetFloat("Trigger", triggervalue);
+            handAnimator.SetFloat("Trigger", triggerValue);
+
         }
         else
         {
             handAnimator.SetFloat("Trigger", 0);
         }
-        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripvalue))
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
         {
-            handAnimator.SetFloat("Grip", gripvalue);
+            handAnimator.SetFloat("Grip", gripValue);
+
         }
         else
         {
             handAnimator.SetFloat("Grip", 0);
         }
-    }
 
-    /*
-    private void testInput()
-    {
-        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
-        {
-            Debug.Log("PrimaryButton [A] is pressed");
-        }
-        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.2f)
-        {
-            Debug.Log("Trigger was pressed more than 20%");
-        }
-        if (targetDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool trigger) && trigger)
-        {
-            Debug.Log("Trigger was pressed");
-        }
-        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 movement) && movement != Vector2.zero)
-        {
-            Debug.Log("Joystick moved " + movement);
-        }
     }
-    */
 }
--- Assets/Scripts/sinseungwoo/escape.cs
+++ Assets/Scripts/sinseungwoo/escape.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class escape : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class escape : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             Debug.Log("Cleared!");
+            SceneManager.LoadScene(0);
         }
     }
 }
